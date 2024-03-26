b5bd61c126f911c23605f070c2bbc748d1d2c1be
Palabola <hodobalazs@gmail.com>
2024-03-26 14:39:36 +0100

    landing page part 1

--- src/app/pages/landingpage/landingpage.component.ts
+++ src/app/pages/landingpage/landingpage.component.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-landingpage',
-  standalone: true,
-  imports: [],
   templateUrl: './landingpage.component.html',
   styleUrl: './landingpage.component.scss'
 })
