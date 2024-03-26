d65138610e2230d452ebfa9fa07763d3e043cb69
Palabola <hodobalazs@gmail.com>
2024-03-26 14:37:24 +0100

    added landing page route, and page layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { LandingpageComponent } from './pages/landingpage/landingpage.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: LandingpageComponent },
+  { path: '**', redirectTo: '' }
+];
