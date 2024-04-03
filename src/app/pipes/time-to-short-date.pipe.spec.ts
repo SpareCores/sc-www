26c4e665fd769fbd4dc6203e99fb17c21b4092f5
Palabola <hodobalazs@gmail.com>
2024-04-03 12:24:40 +0200

    time to date pipe added

---
+++ src/app/pipes/time-to-short-date.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { TimeToShortDatePipe } from './time-to-short-date.pipe';
+
+describe('TimeToShortDatePipe', () => {
+  it('create an instance', () => {
+    const pipe = new TimeToShortDatePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
