47ae371acaa32873f85a8495e17346d5bbafcded
Palabola <hodobalazs@gmail.com>
2024-04-12 13:07:21 +0200

    added /server page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { LandingpageComponent } from './pages/landingpage/landingpage.component';
+import { ServerListingComponent } from './pages/server-listing/server-listing.component';
 
 export const routes: Routes = [
   { path: '', component: LandingpageComponent },
+  { path: 'servers', component: ServerListingComponent },
   { path: '**', redirectTo: '' }
 ];
---
+++ src/app/pages/server-listing/server-listing.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-server-listing',
+  standalone: true,
+  imports: [],
+  templateUrl: './server-listing.component.html',
+  styleUrl: './server-listing.component.scss'
+})
+export class ServerListingComponent {
+
+}
