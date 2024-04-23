5de072cb1a223a49e579ab73d9f2fed36c334389
Palabola <hodobalazs@gmail.com>
2024-04-23 11:04:01 +0200

    disabled storage handler

--- src/app/services/storage-handler.service.ts
+++ src/app/services/storage-handler.service.ts
@@ -6,17 +6,19 @@ import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
 })
 export class StorageHandlerService {
 
+  disabled = true;
+
   constructor(@Inject(PLATFORM_ID) private platformId: object) { }
 
 
   get(key: string): any {
-    if(isPlatformBrowser(this.platformId) && localStorage) {
+    if(!this.disabled && isPlatformBrowser(this.platformId) && localStorage) {
       return localStorage.getItem(key);
     }
   }
 
   set(key: string, value: any): void {
-    if(isPlatformBrowser(this.platformId) && localStorage) {
+    if(!this.disabled && isPlatformBrowser(this.platformId) && localStorage) {
       localStorage.setItem(key, value);
     }
   }
