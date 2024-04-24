703e8a69d55ff68b24c924570d8e330ce1c6f02c
Palabola <hodobalazs@gmail.com>
2024-04-24 15:54:28 +0200

    update canonical URL after navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { isPlatformBrowser } from '@angular/common';
+import { DOCUMENT, isPlatformBrowser } from '@angular/common';
 import { Component, Inject, PLATFORM_ID } from '@angular/core';
+import { Meta } from '@angular/platform-browser';
+import { NavigationEnd, NavigationError, NavigationStart, Router, Event } from '@angular/router';
 import { initFlowbite } from 'flowbite';
 import { register } from 'swiper/element/bundle';
 
@@ -11,8 +13,31 @@ import { register } from 'swiper/element/bundle';
 export class AppComponent {
   title = 'sc-www';
 
-  constructor(@Inject(PLATFORM_ID) private platformId: object,) {
+  constructor(@Inject(PLATFORM_ID) private platformId: object,
+    @Inject(DOCUMENT) private document: Document,
+    private router: Router,
+    private metaTagService: Meta) {
 
+      router.events.subscribe( (event: Event) => {
+
+        if (event instanceof NavigationStart) {
+            // Show loading indicator
+        }
+
+        if (event instanceof NavigationEnd) {
+          let url = 'https://sparecores.com';
+          if(event?.urlAfterRedirects?.length > 1) {
+            url += event?.urlAfterRedirects.split('?')[0];
+          }
+          this.updateCanonical(url.toLowerCase());
+        }
+
+        if (event instanceof NavigationError) {
+            // Hide loading indicator
+
+            // Present error to user
+        }
+    });
   }
 
   ngOnInit() {
@@ -20,5 +45,19 @@ export class AppComponent {
     if (isPlatformBrowser(this.platformId)) {
       initFlowbite();
     }
+  }
+
+  updateCanonical(url: string) {
+    const canonicalUrl = url;
+    const head = this.document.getElementsByTagName('head')[0];
+    var element: HTMLLinkElement | null = this.document.querySelector(`link[rel='canonical']`) || null;
+    if (element == null) {
+      element = this.document.createElement('link') as HTMLLinkElement;
+      head.appendChild(element);
+    }
+    element.setAttribute('rel', 'canonical');
+    element.setAttribute('href', canonicalUrl);
+
+    this.metaTagService.updateTag({ property: 'og:url', content: canonicalUrl }, "property='og:url'");
   }
 }
