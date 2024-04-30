9c7cfa22679a7e4c05efc7e68f14aa45acb768d0
Gergely Daroczi (@daroczig) <daroczig@sparecores.com>
2024-04-30 14:06:40 +0200

    update to demo app

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,3 +1,9 @@
 import { AppServerModule } from './app/app.server.module';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from './app/app.component';
+import { config } from './app/app.config.server';
 
+const bootstrap = () => bootstrapApplication(AppComponent, config);
+
+export default bootstrap;
 export default AppServerModule;
