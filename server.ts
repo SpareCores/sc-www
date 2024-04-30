be96754ee1acf28e27b852ae258740d0827ca273
Gergely Daroczi (@daroczig) <daroczig@sparecores.com>
2024-04-30 13:55:10 +0200

    use AppServerModule

--- server.ts
+++ server.ts
@@ -3,7 +3,7 @@ import { CommonEngine } from '@angular/ssr';
 import express from 'express';
 import { fileURLToPath } from 'node:url';
 import { dirname, join, resolve } from 'node:path';
-import bootstrap from './src/main.server';
+import { AppServerModule } from './src/app/app.server.module';
 
 // The Express app is exported so that it can be used by serverless Functions.
 export function app(): express.Express {
@@ -12,7 +12,7 @@ export function app(): express.Express {
   const browserDistFolder = resolve(serverDistFolder, '../browser');
   const indexHtml = join(serverDistFolder, 'index.server.html');
 
-  const commonEngine = new CommonEngine();
+  const commonEngine = new CommonEngine({bootstrap: AppServerModule});
 
   server.set('view engine', 'html');
   server.set('views', browserDistFolder);
@@ -30,7 +30,7 @@ export function app(): express.Express {
 
     commonEngine
       .render({
-        bootstrap,
+        AppServerModule,
         documentFilePath: indexHtml,
         url: `${protocol}://${headers.host}${originalUrl}`,
         publicPath: browserDistFolder,
