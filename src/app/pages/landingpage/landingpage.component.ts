0b52502c01f1d241c590e6278b18b7782c27a3ed
Palabola <hodobalazs@gmail.com>
2024-05-06 13:08:56 +0200

    bug fixes

--- src/app/pages/landingpage/landingpage.component.ts
+++ src/app/pages/landingpage/landingpage.component.ts
@@ -144,7 +144,7 @@ export class LandingpageComponent {
   welcomeAnim(startingDelay: number = 1000) {
 
     // get the cheapest machine
-    this.keeperAPI.searchServers({vcpus_min: this.cpuCount, memory_min: this.ramCount * 1024, limit: 100}).then(servers => {
+    this.keeperAPI.searchServers({vcpus_min: this.cpuCount, memory_min: this.ramCount, limit: 100}).then(servers => {
 
       if(!this.spinnerClicked) {
 
@@ -207,7 +207,7 @@ export class LandingpageComponent {
       spinButton.style.animation = 'press 2.5s';
     }
 
-    this.keeperAPI.searchServers({vcpus_min: this.cpuCount, memory_min: this.ramCount * 1024, limit: 100}).then(servers => {
+    this.keeperAPI.searchServers({vcpus_min: this.cpuCount, memory_min: this.ramCount, limit: 100}).then(servers => {
       console.log('Servers:', servers);
       this.spinAnim(servers.body);
     }).catch(err => {
