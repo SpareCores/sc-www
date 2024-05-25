e475d350e36b1d8ffc4d028f16d697d6edd8f479
Gergely Daroczi (@daroczig) <daroczig@sparecores.com>
2024-05-25 21:32:12 +0200

    import instead of require

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -1,6 +1,7 @@
 
 import * as process from 'process';
+import { Buffer } from 'buffer';
 
 (window as any).global = window;
-(window as any).Buffer = (window as any).Buffer || require('buffer').Buffer;
+(window as any).Buffer = (window as any).Buffer || Buffer;
 (window as any).process = process;
