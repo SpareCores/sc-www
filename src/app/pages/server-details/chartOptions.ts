089984d067d6d560ca4f1b9df86b3d6f4e2e7f6e
Palabola <hodobalazs@gmail.com>
2024-06-05 11:43:53 +0200

    cleanup

--- src/app/pages/server-details/chartOptions.ts
+++ src/app/pages/server-details/chartOptions.ts
@@ -67,8 +67,6 @@ export const barChartDataEmpty: ChartData<'bar'> = {
   ],
 };
 
-
-
 export const radarChartOptions: ChartConfiguration<'radar'>['options'] = {
   scales: {
     r: {
@@ -97,16 +95,6 @@ export const radarChartOptions: ChartConfiguration<'radar'>['options'] = {
       },
     }
   },
-};
-
-export const radarChartDataEmpty: ChartData<'radar'> = {
-  labels: [],
-  datasets: [
-    { data: [1,2,3,4,5], label: 'data1', borderColor: '#34D399', backgroundColor: '#34D39933'},
-    { data: [5,6,7,8,9], label: 'data2', borderColor: '#E5E7EB', backgroundColor: '#E5E7EB33'},
-    { data: [11,12,13,14,15], label: 'data3', borderColor: '#38BDF8', backgroundColor: '#38BDF833'},
-    { data: [12,13,14,9,7], label: 'data4', borderColor: '#FACC15', backgroundColor: '#FACC1533'},
-  ],
 };
 
 export const radatDatasetColors = [
