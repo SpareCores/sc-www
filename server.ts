33cc617e0e36a565e05816825c2f731e41658fa6
Palabola <hodobalazs@gmail.com>
2024-06-24 22:28:59 +0200

    thumbnail update with baseUrl detection

--- server.ts
+++ server.ts
@@ -4,6 +4,7 @@ import express from 'express';
 import { fileURLToPath } from 'node:url';
 import { dirname, join, resolve } from 'node:path';
 import bootstrap from './src/main.server';
+import { REQUEST, RESPONSE } from './src/express.tokens';
 //import bootstrap from './src/main.server';
 
 // The Express app is exported so that it can be used by serverless Functions.
@@ -35,7 +36,11 @@ export function app(): express.Express {
         documentFilePath: indexHtml,
         url: `${protocol}://${headers.host}${originalUrl}`,
         publicPath: browserDistFolder,
-        providers: [{ provide: APP_BASE_HREF, useValue: baseUrl }],
+        providers: [
+          { provide: APP_BASE_HREF, useValue: baseUrl },
+          { provide: RESPONSE, useValue: res },
+          { provide: REQUEST, useValue: req },
+        ],
       })
       .then((html) => res.send(html))
       .catch((err) => next(err));
