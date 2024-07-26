bd95954be0aa1a3036b3a73a0db3e25abb9fdb9a
Palabola <hodobalazs@gmail.com>
2024-07-26 16:42:27 +0200

    CSS

--- src/app/pages/tos/tos.component.ts
+++ src/app/pages/tos/tos.component.ts
@@ -7,17 +7,19 @@ import { MarkdownModule, MarkdownService } from 'ngx-markdown';
 import { BreadcrumbSegment, BreadcrumbsComponent } from '../../components/breadcrumbs/breadcrumbs.component';
 import { DomSanitizer } from '@angular/platform-browser';
 import * as yaml from 'js-yaml';
+import { TimeToShortDatePipe } from '../../pipes/time-to-short-date.pipe';
 
 @Component({
   selector: 'app-tos',
   standalone: true,
-  imports: [BreadcrumbsComponent, RouterModule, CommonModule, HttpClientModule, MarkdownModule],
+  imports: [BreadcrumbsComponent, RouterModule, CommonModule, HttpClientModule, MarkdownModule, TimeToShortDatePipe],
   templateUrl: './tos.component.html',
   styleUrl: './tos.component.scss'
 })
 export class TOSComponent implements OnInit {
 
   articleBody: any;
+  articleMeta: any;
 
   breadcrumbs: BreadcrumbSegment[] = [
     { name: 'Home', url: '/' },
@@ -38,20 +40,20 @@ export class TOSComponent implements OnInit {
 
       this.http.get(`./assets/legal/${id}.md`, { responseType: 'text' }).subscribe((file: any) => {
 
-          // Assuming `file` is a string containing your Markdown content...
-          const match = /---\r?\n([\s\S]+?)\r?\n---/.exec(file);
-          if(match === null) {
-            return;
-          }
-          const data: any = yaml.load(match[1]);
-          const content = file.replace(/---\r?\n[\s\S]+?\r?\n---/, '');
+        // Assuming `file` is a string containing your Markdown content...
+        const match = /---\r?\n([\s\S]+?)\r?\n---/.exec(file);
+        if(match === null) {
+          return;
+        }
+        this.articleMeta = yaml.load(match[1]);
+        const content = file.replace(/---\r?\n[\s\S]+?\r?\n---/, '');
 
         this.articleBody = this.domSanitizer.bypassSecurityTrustHtml(this.markdownService.parse(content) as string);
 
         this.breadcrumbs = [
           { name: 'Home', url: '/' },
           { name: 'Legal', url: '/legal' },
-          { name: data.title, url: `/legal/${id}` }
+          { name: this.articleMeta.title, url: `/legal/${id}` }
         ];
       });
 
