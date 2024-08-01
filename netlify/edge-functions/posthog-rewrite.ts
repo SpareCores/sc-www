31f369b2aae205cd03ee6560ba40e4f61123599c
Gergely Daroczi (@daroczig) <daroczig@sparecores.com>
2024-08-01 22:38:27 +0200

    netlify edge fn to proxy the PostHog requests

---
+++ netlify/edge-functions/posthog-rewrite.ts
@@ -0,0 +1,30 @@
+import type { Config, Context } from "@netlify/edge-functions";
+
+// [[redirects]]
+//   from = "/ingest/static/*"
+//   to = "https://eu-assets.i.posthog.com/static/:splat"
+//   host = "eu-assets.i.posthog.com"
+//   status = 200
+//   force = true
+
+// [[redirects]]
+//   from = "/ingest/*"
+//   to = "https://eu.i.posthog.com/:splat"
+//   host = "eu.i.posthog.com"
+//   status = 200
+//   force = true
+
+export default async (request: Request, context: Context) => {
+  const url = new URL(request.url);
+  if (url.pathname.startsWith('/pgp/')) {
+    const pgOrigin = url.pathname.startsWith('/pgp/static/') ? 'https://eu-assets.i.posthog.com' : 'https://eu.i.posthog.com';
+    const pgUrl = pgOrigin + url.pathname.replace('/pgp/', '/')
+    return new URL("/subscriber-sale", request.url);
+  }
+  return fetch(request);
+};
+
+export const config: Config = {
+  // PostHog Proxy
+  path: "/pgp/*",
+};
