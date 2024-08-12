d88921693c5d2722bf657a9fa10c1243e3bf3c8e
Palabola <hodobalazs@gmail.com>
2024-08-12 13:11:44 +0200

    opt-out /table/server from stateManager

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,10 +3,17 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withHttpTransferCacheOptions } from '@angular/platform-browser';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import { HttpRequest, provideHttpClient, withFetch } from '@angular/common/http';
 import { ArrowDownNarrowWide, ArrowDownWideNarrow, BookText, Clipboard, Box, Check, ChevronDown, ChevronLeft, ChevronRight, Codesandbox, Cpu, Database, DollarSign, Facebook, Github, Home, Hotel, Linkedin, LucideAngularModule, MemoryStick, PcCase, Search, Server, SquareKanban, Twitter, User, Building2, Heater, CandlestickChart, MapPinned, Scale, Ellipsis, Menu, Leaf, ShoppingCart, ChevronUp, ExternalLink, Info, ClipboardCheck, ScrollText, Youtube, Trash, CircleX, X, CircleArrowDown, CircleArrowUp } from 'lucide-angular';
 import { MarkdownModule } from 'ngx-markdown';
 import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
+
+function httpFilter(req: HttpRequest<any>): boolean {
+  if(req.url.includes('table/server')) {
+    return false;
+  }
+  return req.method === 'GET';
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -14,6 +21,7 @@ export const appConfig: ApplicationConfig = {
     provideClientHydration(
       withHttpTransferCacheOptions({
         includeHeaders: ['x-total-count', 'x-request-id'],
+        filter: httpFilter,
       }),
     ),
     provideHttpClient(withFetch()),
