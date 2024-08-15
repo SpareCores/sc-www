ad7a1ee02fdb308b8fd0a20c15e2fd359e3e6486
Palabola <hodobalazs@gmail.com>
2024-08-15 14:20:58 +0200

    typo

--- src/app/pages/server-compare/server-compare.component.ts
+++ src/app/pages/server-compare/server-compare.component.ts
@@ -533,7 +533,7 @@ export class ServerCompareComponent implements OnInit, AfterViewInit {
     this.servers?.forEach((s: ServerPKsWithPrices) => {
       const temp = s.benchmark_scores?.find((b) =>
         b.benchmark_id === 'stress_ng:cpu_all' &&
-        ((isMulti && server.vcpus && server.vcpus > 1) ? ((b.config as any)?.cores > 1) :(b.config as any)?.cores === 1))?.score || 0;
+        ((isMulti && s.vcpus && s.vcpus > 1) ? ((b.config as any)?.cores > 1) :(b.config as any)?.cores === 1))?.score || 0;
       if(temp > prop) {
         isBest = false;
       }
