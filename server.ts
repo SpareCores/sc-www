4f7267f5679e00f55c1b9276dac7c619d3c78937
Palabola <hodobalazs@gmail.com>
2024-08-26 14:44:54 +0200

    use sentry/node

--- server.ts
+++ server.ts
@@ -1,3 +1,30 @@
+import * as Sentry from "@sentry/node";
+
+const SENTRY_DSN = import.meta?.env?.NG_APP_SENTRY_DSN_NODE;
+const SENTRY_TRACE_SAMPLE_RATE = import.meta?.env?.NG_APP_SENTRY_TRACE_SAMPLE_RATE || '0';
+const SENTRY_PROFILE_SAMPLE_RATE = import.meta?.env?.NG_APP_SENTRY_PROFILE_SAMPLE_RATE || '0';
+const SENTRY_ENVIRONMENT = import.meta?.env?.NG_APP_SENTRY_ENVIRONMENT || 'development';
+const SENTRY_RELEASE = import.meta?.env?.NG_APP_SENTRY_RELEASE || undefined;
+
+const BACKEND_BASE_URI = import.meta.env['NG_APP_BACKEND_BASE_URI'];
+
+
+if(SENTRY_DSN && SENTRY_DSN !== '') {
+  Sentry.init({
+    dsn: SENTRY_DSN,
+    environment: SENTRY_ENVIRONMENT + 'server',
+    release: SENTRY_RELEASE,
+
+    integrations: [
+      Sentry.httpIntegration(),
+      Sentry.expressIntegration(),
+    ],
+    tracePropagationTargets: ['localhost', /^\//, BACKEND_BASE_URI],
+    tracesSampleRate: Number(SENTRY_TRACE_SAMPLE_RATE),
+    profilesSampleRate: Number(SENTRY_PROFILE_SAMPLE_RATE),
+  });
+}
+
 import { APP_BASE_HREF } from '@angular/common';
 import { CommonEngine } from '@angular/ssr';
 import express from 'express';
@@ -5,7 +32,7 @@ import { fileURLToPath } from 'node:url';
 import { dirname, join, resolve } from 'node:path';
 import bootstrap from './src/main.server';
 import { REQUEST, RESPONSE } from './src/express.tokens';
-//import bootstrap from './src/main.server';
+
 
 // The Express app is exported so that it can be used by serverless Functions.
 export function app(): express.Express {
@@ -15,6 +42,8 @@ export function app(): express.Express {
   const indexHtml = join(serverDistFolder, 'index.server.html');
 
   const commonEngine = new CommonEngine();
+
+  Sentry.setupExpressErrorHandler(server);
 
   server.set('view engine', 'html');
   server.set('views', browserDistFolder);
@@ -30,6 +59,29 @@ export function app(): express.Express {
   server.get('*', (req, res, next) => {
     const { protocol, originalUrl, baseUrl, headers } = req;
 
+    Sentry.startSpan(
+      { name: `${originalUrl}`,
+        op: 'render',
+        forceTransaction: true },
+      async () => {
+        const result = await commonEngine
+          .render({
+            bootstrap,
+            documentFilePath: indexHtml,
+            url: `${protocol}://${headers.host}${originalUrl}`,
+            publicPath: browserDistFolder,
+            providers: [
+              { provide: APP_BASE_HREF, useValue: baseUrl },
+              { provide: RESPONSE, useValue: res },
+              { provide: REQUEST, useValue: req },
+            ],
+          });
+        return result;
+      },
+    ).then((html: any) => res.send(html))
+    .catch((err: any) => next(err));
+
+    /*
     commonEngine
       .render({
         bootstrap,
@@ -44,6 +96,7 @@ export function app(): express.Express {
       })
       .then((html) => res.send(html))
       .catch((err) => next(err));
+      */
   });
 
   return server;
--- src/main.server.ts
+++ src/main.server.ts
@@ -1,28 +1,6 @@
 import { bootstrapApplication } from "@angular/platform-browser";
 import { AppComponent } from "./app/app.component";
 import { config } from "./app/app.config.server";
-import * as Sentry from "@sentry/angular";
-
-const SENTRY_DSN = import.meta?.env?.NG_APP_SENTRY_DSN;
-const SENTRY_TRACE_SAMPLE_RATE = import.meta?.env?.NG_APP_SENTRY_TRACE_SAMPLE_RATE || '0';
-const SENTRY_PROFILE_SAMPLE_RATE = import.meta?.env?.NG_APP_SENTRY_PROFILE_SAMPLE_RATE || '0';
-const SENTRY_ENVIRONMENT = import.meta?.env?.NG_APP_SENTRY_ENVIRONMENT || 'development';
-const SENTRY_RELEASE = import.meta?.env?.NG_APP_SENTRY_RELEASE || undefined;
-
-const BACKEND_BASE_URI = import.meta.env['NG_APP_BACKEND_BASE_URI'];
-
-if(SENTRY_DSN && SENTRY_DSN !== '') {
-  Sentry.init({
-    dsn: SENTRY_DSN,
-    environment: SENTRY_ENVIRONMENT + 'server',
-    release: SENTRY_RELEASE,
-
-    integrations: [Sentry.browserTracingIntegration()],
-    tracePropagationTargets: ['localhost', /^\//, BACKEND_BASE_URI],
-    tracesSampleRate: Number(SENTRY_TRACE_SAMPLE_RATE),
-    profilesSampleRate: Number(SENTRY_PROFILE_SAMPLE_RATE),
-  });
-}
 
 const bootstrap = () => bootstrapApplication(AppComponent, config);
 
