8e12d99af9b086cea36803ed5a782322a48520cf
Gergely Daroczi (@daroczig) <daroczig@sparecores.com>
2024-09-05 22:24:13 +0200

    assert string

--- server.ts
+++ server.ts
@@ -17,7 +17,7 @@ export function app(): express.Express {
       method: req.method,
       path: originalUrl,
       userAgent: headers['user-agent'],
-      ip: headers['x-forwarded-for'] ? headers['x-forwarded-for'].split(',')[0] : ip,
+      ip: headers['x-forwarded-for'] ? (headers['x-forwarded-for'] as string).split(',')[0] : ip,
       country: headers['cloudfront-viewer-country'],
       timestamp: new Date().toISOString()
     };
