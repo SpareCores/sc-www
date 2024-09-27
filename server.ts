92b636d4d49f1cc7a7f3bc1218ac1fda7bc340b3
Gergely Daroczi (@daroczig) <daroczig@sparecores.com>
2024-09-27 13:24:45 +0200

    dynamic cache control

--- server.ts
+++ server.ts
@@ -37,8 +37,23 @@ export function app(): express.Express {
 
   // Serve static files
   server.get('*.*', express.static(browserDistFolder, {
-    maxAge: '1y'
+    setHeaders: (res, path) => {
+      const generatedJsPattern = /-[A-Z0-9]+\.(js|css|woff2)$/;
+      if (generatedJsPattern.test(path)) {
+        // generated files with hashed filenames can be cached forever
+        res.setHeader('Cache-Control', 'public, max-age=31536000, immutable');
+      } else {
+        // default to cache for 1 hour
+        res.setHeader('Cache-Control', 'public, max-age=3600');
+      }
+    }
   }));
+
+  // Cache dynamic content for 10 mins
+  server.use((req, res, next) => {
+    res.setHeader('Cache-Control', 'public, max-age=600');
+    next();
+  });
 
   // All regular routes use the Angular engine
   server.get('*', (req, res, next) => {
