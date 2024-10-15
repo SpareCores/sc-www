f9cf1be114ad99d6cc32eee5b884931cc21ddfc0
Gergely Daroczi (@daroczig) <daroczig@sparecores.com>
2024-10-15 23:49:29 +0200

    compare hcloud servers and example aws comparison

--- src/app/pages/server-compare/special-compares.js
+++ src/app/pages/server-compare/special-compares.js
@@ -1,34 +1,129 @@
 
 const specialCompares = [
   {
-    id: 'test',
-    name: 'Test',
-    title: 'Test title',
-    description: 'Test dewscirption',
+    id: 'hcloud-2vcpus',
+    title: '2 vCPU servers at Hetzner Cloud',
+    description: 'All Hetzner Cloud server types with 2 shared or dedicated vCPUs.',
     instances: [
       {
-        vendor: 'aws',
-        server: 'c6g.medium'
-      },
-      {
         vendor: 'hcloud',
-        server: 'cpx51'
-      }
+        server: 'cx21'
+        },
+        {
+          vendor: 'hcloud',
+          server: 'cx22'
+        },
+        {
+          vendor: 'hcloud',
+          server: 'cpx11'
+        },
+        {
+          vendor: 'hcloud',
+          server: 'cax11'
+        },
+        {
+          vendor: 'hcloud',
+          server: 'ccx13'
+        }
     ]
   },
   {
-    id: 'test2',
-    name: 'Test2',
-    title: 'Test2 title',
-    description: 'Test2 dewscirption. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec pur Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purLorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purLorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purLorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purLorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec purLorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nec pur  ',
+    id: 'hcloud-4vcpus',
+    title: '4 vCPU servers at Hetzner Cloud',
+    description: 'All Hetzner Cloud server types with 2 shared or dedicated vCPUs.',
+    instances: [
+      {
+        vendor: 'hcloud',
+        server: 'cx41'
+        },
+        {
+          vendor: 'hcloud',
+          server: 'cx32'
+        },
+        {
+          vendor: 'hcloud',
+          server: 'cpx31'
+        },
+        {
+          vendor: 'hcloud',
+          server: 'cax21'
+        },
+        {
+          vendor: 'hcloud',
+          server: 'ccx23'
+        }
+    ]
+  },  
+  {
+    id: 'hcloud-8vcpus',
+    title: '8 vCPU servers at Hetzner Cloud',
+    description: 'All Hetzner Cloud server types with 8 shared or dedicated vCPUs.',
+    instances: [
+      {
+        vendor: 'hcloud',
+        server: 'cx51'
+        },
+        {
+          vendor: 'hcloud',
+          server: 'cx42'
+        },
+        {
+          vendor: 'hcloud',
+          server: 'cpx41'
+        },
+        {
+          vendor: 'hcloud',
+          server: 'cax31'
+        },
+        {
+          vendor: 'hcloud',
+          server: 'ccx33'
+        }
+    ]
+  },
+  {
+    id: 'hcloud-16vcpus',
+    title: '16 vCPU servers at Hetzner Cloud',
+    description: 'All Hetzner Cloud server types with 16 shared or dedicated vCPUs.',
+    instances: [
+        {
+          vendor: 'hcloud',
+          server: 'cx52'
+        },
+        {
+          vendor: 'hcloud',
+          server: 'cpx51'
+        },
+        {
+          vendor: 'hcloud',
+          server: 'cax41'
+        },
+        {
+          vendor: 'hcloud',
+          server: 'ccx43'
+        }
+    ]
+  },
+  {
+    id: 'aws-c-large',
+    title: 'C5/C6/C7 Large Instances at AWS',
+    description: '3 generations with mixed x86 and ARM architecture of the large size instances from the C series at AWS.',
     instances: [
       {
         vendor: 'aws',
-        server: 'c6g.medium'
+        server: 'c5.large'
       },
       {
-        vendor: 'hcloud',
-        server: 'cpx51'
+        vendor: 'aws',
+        server: 'c6g.large'
+      },
+      {
+        vendor: 'aws',
+        server: 'c7g.large'
+      },
+      {
+        vendor: 'aws',
+        server: 'c7i.large'
       }
     ]
   }
