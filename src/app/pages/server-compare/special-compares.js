0b72018d9910458ddc69fbf0034b97b7b0bfe08f
Gergely Daroczi (@daroczig) <daroczig@sparecores.com>
2024-10-16 22:10:50 +0200

    2 vcpu best single core performance

--- src/app/pages/server-compare/special-compares.js
+++ src/app/pages/server-compare/special-compares.js
@@ -1,6 +1,42 @@
 
 const specialCompares = [
   {
+    id: 'best-singlecore-2vcpu',
+    title: 'Best single-core performance servers with 2 vCPUs',
+    description: `This is a manually curated list of 2 vCPU servers with the best single-core performance as per stress-ng's <code>div16</code> CPU burning method.
+    Note that servers using the same CPU model at the same vendor were deduped, and only the most general options was kept (e.g. AWS's <code>r6a.large</code>, <code>m6a.large</code>, and <code>c6a.large</code> showing only <code>m6a.large</code> with 8 GiB of memory; similarly GCP's <code>c2d-highmem-2</code>, <code>c2d-standard-2</code>, and <code>c2d-highcpu-2</code> showing only <code>c2d-standard-2</code> with 8 GiB of memory as well).`,
+    instances: [
+      {
+        vendor: 'aws',
+        server: 'm7a.large'
+      },
+      {
+        vendor: 'hcloud',
+        server: 'ccx13'
+      },
+      {
+        vendor: 'aws',
+        server: 'm6a.large'
+      },
+        {
+          vendor: 'gcp',
+          server: 'c2d-standard-2'
+        },
+        {
+          vendor: 'azure',
+          server: 'Standard_D2as_v4'
+        },
+        {
+          vendor: 'azure',
+          server: 'Standard_B2as_v2'
+        },
+        {
+          vendor: 'gcp',
+          server: 't2d-standard-2'
+        }
+    ]
+  },
+  {
     id: 'hcloud-2vcpus',
     title: '2 vCPU servers at Hetzner Cloud',
     description: 'All Hetzner Cloud server types with 2 shared or dedicated vCPUs.',
@@ -30,7 +66,7 @@ const specialCompares = [
   {
     id: 'hcloud-4vcpus',
     title: '4 vCPU servers at Hetzner Cloud',
-    description: 'All Hetzner Cloud server types with 2 shared or dedicated vCPUs.',
+    description: 'All Hetzner Cloud server types with 4 shared or dedicated vCPUs.',
     instances: [
       {
         vendor: 'hcloud',
