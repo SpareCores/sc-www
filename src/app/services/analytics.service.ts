f391febb0049afc9fda81f9d70c41d07de794364
Palabola <hodobalazs@gmail.com>
2024-11-12 15:05:11 +0100

    update posthog

--- src/app/services/analytics.service.ts
+++ src/app/services/analytics.service.ts
@@ -1,6 +1,7 @@
 import { isPlatformBrowser } from '@angular/common';
 import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
 import * as Sentry from "@sentry/angular";
+import posthog from 'posthog-js';
 
 const POSTHOG_KEY = import.meta?.env?.NG_APP_POSTHOG_KEY;
 const POSTHOG_HOST = import.meta?.env?.NG_APP_POSTHOG_HOST;
@@ -22,26 +23,22 @@ export class AnalyticsService {
     }
 
     if (!this.trackingInitialized && POSTHOG_KEY && POSTHOG_HOST && typeof window !== 'undefined' && typeof document !== 'undefined') {
-      /*
       posthog.init(POSTHOG_KEY, {
         api_host: POSTHOG_HOST,
         persistence: 'sessionStorage',
       });
       this.trackingInitialized = true;
-      */
     }
   }
 
   public trackEvent(eventName: string, properties: { [key: string]: any }): void {
     if (this.trackingInitialized) {
-      //posthog.capture(eventName, properties)
-      console.log('Event tracked:', eventName, properties);
+      posthog.capture(eventName, properties)
     }
   }
 
   public getId() {
-    //return posthog.get_distinct_id();
-    return '';
+    return posthog.get_distinct_id();
   }
 
   public SentryException(exception: any, hint?: any) {
