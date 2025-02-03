4983c2fdbf2ff5c38f40b7e59fd0fbe311d6c6b6
Gergely Daroczi (@daroczig) <daroczig@sparecores.com>
2025-02-03 09:37:02 +0100

    drop test special id

--- src/app/pages/server-listing/special-lists.js
+++ src/app/pages/server-listing/special-lists.js
@@ -17,26 +17,14 @@ var specialServerLists = [
   {
     id: 'gpu',
     title: 'GPU Servers',
-    description: `Explore, search, and evaluate GPU-assisted cloud servers from multiple vendors in the table below. This comprehensive list includes diverse attributes such as CPU count, detailed processor information, memory, GPU model and memory amount, storage, network speed and capacity, available operating systems. Use the sidebar to filter the results, or enter your freetext query in the “Search prompt” bar. You can also compare servers by selecting at least two rows using the checkboxes.`,
+    description: 'Explore, search, and evaluate GPU-assisted cloud servers from multiple vendors in the table below. This comprehensive list includes diverse attributes such as CPU count, detailed processor information, memory, GPU model and memory amount, storage, network speed and capacity, available operating systems. Use the sidebar to filter the results, or enter your freetext query in the “Search prompt” bar. You can also compare servers by selecting at least two rows using the checkboxes.',
     parameters: {
       gpu_min: 1
     },
     columns: '300712',
     order_by: 'min_price',
     order_dir: 'desc'
-  },
-  {
-    id: 'benchmark',
-    title: 'Benchmark Servers',
-    description: `Explore, search, and evaluate GPU-assisted cloud servers from multiple vendors in the table below. This comprehensive list includes diverse attributes such as CPU count, detailed processor information, memory, GPU model and memory amount, storage, network speed and capacity, available operating systems. Use the sidebar to filter the results, or enter your freetext query in the “Search prompt” bar. You can also compare servers by selecting at least two rows using the checkboxes.`,
-    parameters: {
-      gpu_min: 1
-    },
-    columns: '1203848',
-    benchmark_config: '{"algo": "gzip", "compression_level": 5, "threads": 1}',
-    benchmark_id: 'compression_text:compress'
   }
-
 ]
 
 module.exports = specialServerLists;
