f00542f65e03c6d0fc346c74954183a9d0b1ac6d
Gergely Daroczi (@daroczig) <daroczig@sparecores.com>
2025-04-15 00:23:49 +0200

    title case

--- src/app/pages/server-details/chartFromBenchmarks.ts
+++ src/app/pages/server-details/chartFromBenchmarks.ts
@@ -97,7 +97,7 @@ export const staticWebSecondaryOptions = [
 
 export const staticWebChartTemplate: ChartFromBenchmarkTemplate = {
   id: 'static_web',
-  name: 'Static web server',
+  name: 'Static Web Server',
   options: staticWebChartTemplateOptions,
   secondaryOptions: staticWebSecondaryOptions,
   selectedOption: 0,
@@ -108,7 +108,7 @@ export const staticWebChartTemplate: ChartFromBenchmarkTemplate = {
 
 export const staticWebChartCompareTemplate: ChartFromBenchmarkTemplate = {
   id: 'static_web',
-  name: 'Static web server',
+  name: 'Static Web Server',
   options: staticWebChartTemplateOptions2,
   secondaryOptions: staticWebSecondaryOptions,
   selectedOption: 0,
