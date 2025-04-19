c05b6c3ce1cf2f5d0d3ba9f48e465515f3697e11
Gergely Daroczi (@daroczig) <daroczig@sparecores.com>
2025-04-19 10:52:58 +0200

    add search pages for shared/burstable/dedicated servers

--- src/app/pages/server-listing/special-lists.js
+++ src/app/pages/server-listing/special-lists.js
@@ -15,6 +15,30 @@
 
 var specialServerLists = [
   {
+    id: 'shared',
+    title: 'Shared-Core Cloud Servers',
+    description: 'Explore, search, and evaluate shared-core cloud servers from multiple vendors in the table below. This comprehensive list includes diverse attributes such as CPU count, detailed processor information, memory, GPU model and memory amount, storage, network speed and capacity, available operating systems. Use the sidebar to filter the results, or enter your freetext query in the "Search prompt" bar. You can also compare servers by selecting at least two rows using the checkboxes.',
+    parameters: {
+      cpu_allocation: 'Shared'
+    },
+  },
+  {
+    id: 'burstable',
+    title: 'Burstable Cloud Servers',
+    description: 'Explore, search, and evaluate burstable cloud servers from multiple vendors in the table below. This comprehensive list includes diverse attributes such as CPU count, detailed processor information, memory, GPU model and memory amount, storage, network speed and capacity, available operating systems. Use the sidebar to filter the results, or enter your freetext query in the "Search prompt" bar. You can also compare servers by selecting at least two rows using the checkboxes.',
+    parameters: {
+      cpu_allocation: 'Burstable'
+    },
+  },
+  {
+    id: 'dedicated',
+    title: 'Dedicated Cloud Servers',
+    description: 'Explore, search, and evaluate dedicated cloud servers from multiple vendors in the table below. This comprehensive list includes diverse attributes such as CPU count, detailed processor information, memory, GPU model and memory amount, storage, network speed and capacity, available operating systems. Use the sidebar to filter the results, or enter your freetext query in the "Search prompt" bar. You can also compare servers by selecting at least two rows using the checkboxes.',
+    parameters: {
+      cpu_allocation: 'Dedicated'
+    },
+  },
+  {
     id: 'gpu',
     title: 'GPU Servers',
     description: 'Explore, search, and evaluate GPU-assisted cloud servers from multiple vendors in the table below. This comprehensive list includes diverse attributes such as CPU count, detailed processor information, memory, GPU model and memory amount, storage, network speed and capacity, available operating systems. Use the sidebar to filter the results, or enter your freetext query in the “Search prompt” bar. You can also compare servers by selecting at least two rows using the checkboxes.',
