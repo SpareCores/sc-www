384016b120398d4d392bcfef6b4b93e183642ee4
Gergely Daroczi (@daroczig) <daroczig@sparecores.com>
2025-07-26 00:01:58 +0200

    cleanup moved to navigator

--- src/app/pages/landingpage/landingpage.component.ts
+++ src/app/pages/landingpage/landingpage.component.ts
@@ -25,93 +25,6 @@ import { PrismService } from '../../services/prism.service';
 })
 export class LandingpageComponent implements OnInit, AfterViewInit {
 
-  vendors: any[] = [
-    '✅ Amazon Web Services (Done)',
-    '✅ Google Cloud Platform (Done)',
-    '✅ Hetzner Cloud (Done)',
-    '✅ Microsoft Azure (Done)',
-    '✅ UpCloud (Done)',
-    '⚙️ Vultr (In Progress)',
-    '🗓️ Oracle Cloud Infrastructure (Planned)',
-    '🗓️ Scaleway (Planned)',
-    '🗓️ Alibaba Cloud (Planned)',
-    '🗓️ OVH Cloud (Planned)',
-  ];
-
-  features: any[] = [
-    {
-      count: '353',
-      text: 'availability zones'
-    },
-    {
-      count: '2344',
-      text: 'server types'
-    },
-    {
-      count: '970,000+',
-      text: 'benchmark scores'
-    },
-    {
-      count: '~310,000',
-      text: 'live price records'
-    },
-    {
-      count: '~5k',
-      text: 'records updated hourly'
-    },
-
-    {
-      count: '~45M',
-      text: 'historical records'
-    },
-  ];
-
-  gitHubComponents: any[] = [
-    {
-      component: 'SC Crawler',
-      status: 'Beta',
-      github: 'sc-crawler',
-      pypi: 'sparecores-crawler',
-      docs: 'https://sparecores.github.io/sc-crawler',
-      description: 'Inventory cloud resources into a SQlite database.',
-    },
-    {
-      component: 'SC Inspector',
-      status: 'Beta',
-      description: 'Inspect and benchmark cloud resources.',
-      github: 'sc-inspector',
-      data: 'https://github.com/SpareCores/sc-inspector-data',
-    },
-    {
-      component: 'SC Data',
-      status: 'Beta',
-      github: 'sc-data',
-      pypi: 'sparecores-data',
-      description: 'Wrapper around data collected using the Crawler.',
-      data: 'https://sc-data-public-40e9d310.s3.amazonaws.com/sc-data-all.db.bz2',
-    },
-    {
-      component: 'SC Keeper',
-      status: 'Alpha',
-      description: 'API to search the Data.',
-      github: 'sc-keeper',
-      docs: 'https://keeper.sparecores.net/docs',
-      api: 'https://keeper.sparecores.net',
-    },
-    {
-      component: 'SC Scanner',
-      status: 'Stable',
-      description: 'Web frontend and programming language SDKs for Keeper.',
-      www: 'https://github.com/SpareCores/sc-www',
-    },
-    {
-      component: 'SC Runner',
-      status: 'Beta',
-      description: 'Launching actual cloud instances.',
-      github: 'sc-runner',
-    }
-  ];
-
   featuredArticles: ArticleMeta[] = [];
 
   priceValue = '$0.00150';
@@ -371,22 +284,6 @@ export class LandingpageComponent implements OnInit, AfterViewInit {
       this.hasRealValues = true;
       this.analyticsService.trackEvent('slot machine finished', {'autostarted': isFake});
     }, spinAnimEnd)
-  }
-
-  showTooltip(el: any) {
-    const tooltip = this.tooltip.nativeElement;
-    const scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
-    tooltip.style.left = `${el.target.getBoundingClientRect().left - 25}px`;
-    tooltip.style.top = `${el.target.getBoundingClientRect().bottom + 5 + scrollPosition}px`;
-
-    tooltip.style.display = 'block';
-    tooltip.style.opacity = '1';
-  }
-
-  hideTooltip() {
-    const tooltip = this.tooltip.nativeElement;
-    tooltip.style.display = 'none';
-    tooltip.style.opacity = '0';
   }
 
   toggleQuote(event: Event) {
