c74a2527554d8c65359e75785546ef6b8f488c61
marto <marton.szak@gmail.com>
2026-02-04 09:44:25 +0100

    add netlifyCommonEngineHandler to server.ts Netlify compatibility

--- server.ts
+++ server.ts
@@ -11,6 +11,20 @@ import nodemailer from "nodemailer";
 import handlebars from "handlebars";
 import rateLimit from "express-rate-limit";
 import https from "node:https";
+import { render } from "@netlify/angular-runtime/common-engine";
+
+const commonEngine = new CommonEngine();
+
+export async function netlifyCommonEngineHandler(): Promise<Response> {
+  // Example API endpoints can be defined here.
+  // Uncomment and define endpoints as necessary.
+  // const pathname = new URL(request.url).pathname;
+  // if (pathname === '/api/hello') {
+  //   return Response.json({ message: 'Hello from the API' });
+  // }
+
+  return await render(commonEngine);
+}
 
 const serverDistFolder = dirname(fileURLToPath(import.meta.url));
 //const browserDistFolder = resolve(serverDistFolder, '../browser');
