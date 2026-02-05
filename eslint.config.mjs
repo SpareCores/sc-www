ea64210dc9d34b9f3d11bfbba4d9e1e1ef97aa12
marto <marton.szak@gmail.com>
2026-02-05 14:43:14 +0100

    migrate .eslintrc configurations to new flat config

---
+++ eslint.config.mjs
@@ -0,0 +1,73 @@
+import { defineConfig, globalIgnores } from "eslint/config";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+export default defineConfig([
+  globalIgnores(["projects/**/*"]),
+  {
+    files: ["**/*.ts"],
+
+    extends: compat.extends(
+      "eslint:recommended",
+      "plugin:@typescript-eslint/recommended",
+      "plugin:@angular-eslint/recommended",
+      "plugin:@angular-eslint/template/process-inline-templates",
+    ),
+
+    rules: {
+      "@typescript-eslint/no-explicit-any": ["off"],
+
+      "@angular-eslint/directive-selector": [
+        "error",
+        {
+          type: "attribute",
+          prefix: "app",
+          style: "camelCase",
+        },
+      ],
+
+      "@angular-eslint/component-selector": [
+        "error",
+        {
+          type: "element",
+          prefix: "app",
+          style: "kebab-case",
+        },
+      ],
+
+      "prefer-const": "off",
+    },
+  },
+  {
+    files: ["**/*.html"],
+
+    extends: compat.extends(
+      "plugin:@angular-eslint/template/recommended",
+      "plugin:@angular-eslint/template/accessibility",
+    ),
+
+    rules: {
+      "@angular-eslint/template/click-events-have-key-events": "off",
+      "@angular-eslint/template/interactive-supports-focus": "off",
+      "@angular-eslint/template/no-autofocus": "off",
+    },
+  },
+  {
+    files: ["src/assets/slides/*.html"],
+
+    rules: {
+      "@angular-eslint/template/elements-content": "off",
+      "@angular-eslint/template/alt-text": "off",
+    },
+  },
+]);
