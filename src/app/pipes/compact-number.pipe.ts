55a0fb1a54a9e80e3b4b1e6239ee4784d520cf0a
marto <marton.szak@gmail.com>
2026-02-24 15:07:22 +0100

    create CompactNumberPipe for displaying benchmark numbers

---
+++ src/app/pipes/compact-number.pipe.ts
@@ -0,0 +1,26 @@
+import { Pipe, PipeTransform } from "@angular/core";
+
+const formatterCompact = new Intl.NumberFormat("en-US", {
+  notation: "compact",
+  compactDisplay: "short",
+  minimumFractionDigits: 2,
+  maximumFractionDigits: 2,
+});
+
+const formatterSmall = new Intl.NumberFormat("en-US", {
+  notation: "compact",
+  compactDisplay: "short",
+  maximumSignificantDigits: 4,
+});
+
+@Pipe({
+  name: "compactNumber",
+  standalone: true,
+})
+export class CompactNumberPipe implements PipeTransform {
+  transform(value: number | string): string {
+    const num = typeof value === "string" ? parseFloat(value) : value;
+    if (!num || num <= 0) return "-";
+    return (num < 1 ? formatterSmall : formatterCompact).format(num);
+  }
+}
