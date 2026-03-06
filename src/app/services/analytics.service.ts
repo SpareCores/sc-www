c2d17b74fc705905b2bebdeb30112cf35fcceb8a
marto <marton.szak@gmail.com>
2026-03-06 17:46:22 +0100

    add env variables across the code where used

--- src/app/services/analytics.service.ts
+++ src/app/services/analytics.service.ts
@@ -3,8 +3,8 @@ import { Injectable, PLATFORM_ID, inject } from "@angular/core";
 import * as Sentry from "@sentry/angular";
 import posthog from "posthog-js";
 
-const POSTHOG_KEY = import.meta?.env?.NG_APP_POSTHOG_KEY;
-const POSTHOG_HOST = import.meta?.env?.NG_APP_POSTHOG_HOST;
+const POSTHOG_KEY = import.meta.env.NG_APP_POSTHOG_KEY;
+const POSTHOG_HOST = import.meta.env.NG_APP_POSTHOG_HOST;
 
 const SENTRY_DSN = import.meta.env.NG_APP_SENTRY_DSN;
 
