9ce494f2d1666783ee22d9c5603f182bb10812a4
marto <marton.szak@gmail.com>
2026-03-19 16:01:25 +0100

    remove unused Tick import from memory chart builder service

--- src/app/components/charts/memory/memory-chart-builder.service.ts
+++ src/app/components/charts/memory/memory-chart-builder.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@angular/core";
-import { Tick, TooltipItem, TooltipModel } from "chart.js";
+import { TooltipItem, TooltipModel } from "chart.js";
 import {
   legacyBWMemOperationLabels,
   membenchSeriesLabels,
