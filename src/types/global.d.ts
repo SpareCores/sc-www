f8cbb81730bae9d5a7f53427ce9c7d61c730c3c0
marto <marton.szak@gmail.com>
2026-03-19 12:08:21 +0100

    add ImportMetaEnv and ImportMeta interfaces to global types

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,6 +1,22 @@
 export {};
 
 declare global {
+  interface ImportMetaEnv {
+    NG_APP_BACKEND_BASE_URI?: string;
+    NG_APP_BACKEND_BASE_URI_SSR?: string;
+    NG_APP_POSTHOG_HOST?: string;
+    NG_APP_POSTHOG_KEY?: string;
+    NG_APP_SENTRY_DSN?: string;
+    NG_APP_SENTRY_TRACE_SAMPLE_RATE?: string;
+    NG_APP_SENTRY_PROFILE_SAMPLE_RATE?: string;
+    NG_APP_SENTRY_ENVIRONMENT?: string;
+    NG_APP_SENTRY_RELEASE?: string;
+  }
+
+  interface ImportMeta {
+    readonly env: ImportMetaEnv;
+  }
+
   interface Window {
     HSComboBox?: {
       autoInit?: () => void;
