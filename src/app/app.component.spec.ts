812a0a22c8f2cdb58fe6fe1e0d54069c3bab6f18
marto <marton.szak@gmail.com>
2026-03-19 14:07:19 +0100

    remove redundant title check in AppComponent spec

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,8 +26,6 @@ describe("AppComponent", () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector("h1")?.textContent).toContain(
-      "Hello, sc-www",
-    );
+    expect(compiled.querySelector(".app-shell")).not.toBeNull();
   });
 });
