ec45457bdb2f6e093f34797ca46f8da9c4994a4d
martonszak <marton.szak@gmail.com>
2026-06-24 14:49:52 +0200

    refactor(cpu-cache-size.pipe): simplify cache size formatting by utilizing formatCpuCacheSize function

--- src/app/pipes/cpu-cache-size.pipe.ts
+++ src/app/pipes/cpu-cache-size.pipe.ts
@@ -1,20 +1,12 @@
 import { Pipe, PipeTransform } from "@angular/core";
-import { formatValue } from "./pipe-utils";
+import { formatCpuCacheSize } from "./pipe-utils";
 
 @Pipe({
   name: "cpuCacheSize",
   standalone: true,
 })
 export class CpuCacheSizePipe implements PipeTransform {
-  private readonly KIB = 1024;
-  private readonly MIB = 1024 * 1024;
-
   transform(kibibytes: number | null | undefined): string {
-    if (!kibibytes || kibibytes <= 0) return "-";
-    if (kibibytes * 1024 < this.MIB) {
-      return `${kibibytes} KiB`;
-    }
-    const mib = kibibytes / 1024;
-    return `${formatValue(mib)} MiB`;
+    return formatCpuCacheSize(kibibytes);
   }
 }
