f1ad3039e47413d59b5138ba63a7d9d046ffdb61
martonszak <marton.szak@gmail.com>
2026-07-02 14:29:18 +0200

    refactor(footer): replace Lucide icons with app-icon component

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -1,11 +1,12 @@
 import { Component } from "@angular/core";
 import { ThemeTextComponent } from "../../components/theme-text/theme-text.component";
-import { LucideDynamicIcon, LucideMail } from "@lucide/angular";
+
 import { RouterLink } from "@angular/router";
+import { Icon } from "../../components/icon/icon.js";
 
 @Component({
   selector: "app-footer",
-  imports: [ThemeTextComponent, LucideDynamicIcon, LucideMail, RouterLink],
+  imports: [ThemeTextComponent, RouterLink, Icon],
   templateUrl: "./footer.component.html",
   styleUrl: "./footer.component.scss",
 })
